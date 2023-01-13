e3a6f075c243109b79c45647f3b395039cf410ff
Naga-Rajasekhar <rajasekhar9860@gmail.com>
2023-01-13 15:07:43 +0530

    Learn about useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 function App({ name }) {
+  const [details, setDetails] = useState({});
+  const fetchDetails = async () => {
+    const { data } = await axios.get("https://randomuser.me/api/");
+    // console.log(data);
+    const details = data.results[0];
+    setDetails(details);
+    console.log(details);
+    console.log(details.phone);
+  };
+
+  useEffect(() => {
+    fetchDetails();
+  }, []);
+
   return (
     <>
       <h2>Hii - Rajasekhar, from APP Component</h2>
-      <p style={{color : "blue"}}>Hii-{name}</p>
+      <p style={{ color: "blue" }}>Hii-{name}</p>
     </>
   );
 }
